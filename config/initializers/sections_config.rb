47cd85813f0a628a7df3f52f2d7bb1d9d714646a
Rafael Fernandez <rafinsky@gmail.com>
2021-03-17 15:21:39 +0100

    Catchy info form

--- config/initializers/sections_config.rb
+++ config/initializers/sections_config.rb
@@ -3,5 +3,14 @@ SECTION_CONFIG = {
     {name: 'Title', category: :h1},
     {name: 'Subtitle', category: :p},
     {name: 'Image', category: :img}
+  ],
+  catchy_info: [
+    {name: 'Tell us 5 interesting things', category: :list},
+    {name: '', category: :list},
+    {name: '', category: :list},
+    {name: '', category: :list},
+    {name: '', category: :list},
+    {name: 'Subtitle', category: :h2},
+    {name: 'Profile Video', category: :video}
   ]
 }
