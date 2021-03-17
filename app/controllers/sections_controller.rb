24511c4b0cc5b7ac24fc3d7ee7cb990b56b412a2
German B <geuxor@dr.com>
2021-03-17 13:14:56 +0100

    form-stuff

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -4,4 +4,24 @@ class SectionsController < ApplicationController
   def index
     @sections = Section.order(:order)
   end
+
+
+  def new
+    @section = Section.new(name: "hero")
+    @section.elements.build(@section.get_elements)
+  end
+
+  def create
+    @section = Section.new()
+    # @section.user = current_user
+    if @section.save
+      redirect_to section_path(@section)
+    else
+      render :new
+    end
+  end
+
+  def section_params
+    # params.require(:section).permit(:)
+  end
 end
