8788abc4468f516762b7499286a46dc95b338752
Rafael Fernandez <rafinsky@gmail.com>
2021-03-19 11:59:22 +0100

    Connect sections

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :websites, only: [:create, :new]
-  get 'websites/builder', to: 'websites#builder'
+  resources :websites, only: [:create, :new] do
+    get 'builder', to: 'websites#builder'
+  end
   resources :sections, only: [:index, :create, :new]
 end
