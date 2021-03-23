0817a954b0051d2d4963b82993eec57fb4116487
German B <geuxor@dr.com>
2021-03-23 12:57:20 +0100

    style navbar-websitenew

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -4,14 +4,19 @@ export default class extends Controller {
   static targets = ['menu']
 
   connect() {
-    window.addEventListener("scroll", this.onScroll)
+    window.addEventListener("scroll", this.onScroll); 
+    window.addEventListener("turbolinks:render", this.onScroll);
   }
 
   onScroll = (event) => {
     if (window.scrollY) {
       this.menuTarget.classList.add("dark")
+      console.log('yes dark');
+      
     } else {
       this.menuTarget.classList.remove("dark")
+      console.log('no dark');
+      
     }
   }
 }
