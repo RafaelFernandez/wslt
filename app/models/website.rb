ed38fd53b9003f0a51f795875f503eb5a95535c2
German B <geuxor@dr.com>
2021-03-25 12:06:29 +0100

    m

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 class Website < ApplicationRecord
   DOMAIN_OPTIONS = ['englishlanguageteacher.com', 'spanishlanguageteacher.com']
   THEMES = ['kids']
@@ -7,6 +9,13 @@ class Website < ApplicationRecord
   has_many :testimonials, dependent: :destroy
   has_many :sections, dependent: :destroy
   validates :domain, inclusion: { in: DOMAIN_OPTIONS }
+  has_one_attached :photo
+  after_create :set_default_photo
+
+  def set_default_photo
+    file = URI.open("https://euroamerican.dk/photo/wslt/tempdesign1.png")
+    self.photo.attach(io: file, filename: 'default-img.png', content_type: 'image/png')
+  end
 
   def section_named(name)
     sections&.find_by(name: name)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 Website.destroy_all
 Element.destroy_all
 Section.destroy_all
@@ -5,22 +7,30 @@ Website.destroy_all
 Theme.destroy_all
 User.destroy_all
 
-user = User.create(first_name: 'John', last_name: 'Doe', email: 'j@gmail.com', password: 'password')
-
 Theme.create(name: 'dudu')
 Theme.create(name: 'default')
 kids = Theme.create(name: 'kids')
 
 poppy = User.create!(email: "poppy@poppyworld.com", password: "password", first_name: "Poppy", last_name: "World")
 
+file = URI.open("https://euroamerican.dk/photo/wslt/tempdesign1.png")
+
 poppysworld = Website.create!(
   name: "Poppy's World",
   domain: "englishlanguageteacher.com",
   user: poppy, theme: kids
 )
+poppysworld.photo.attach(io: file, filename: 'poppy.png', content_type: 'image/png')
 
 # Website
-website = Website.create(user: user, theme: kids, name: 'My web', domain: 'englishlanguageteacher.com')
+
+web_names = ["Young Learners", "Business", "History", "Nature", "Most Toughest Languages"]
+5.times do |index|
+  file = URI.open("https://euroamerican.dk/photo/wslt/tempdesign#{index + 2}.png")
+  website = Website.create(user: poppy, theme: kids, name: web_names[index], domain: 'englishlanguageteacher.com')
+  website.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+end
+
 # HERO SECTION
 hero = Section.create( name: 'hero', order: 1, website: poppysworld)
 Element.create(name: 'title', value: 'RACHEL POTTER', category: :h1, section: hero)
@@ -28,56 +38,39 @@ Element.create(name: 'subtitle', value: 'Qualified English language teacher', ca
 Element.create(name: 'text', value: 'Helping young learners reach fluency through intuative learning and games', category: :p, section: hero)
 
 # CATCHY INFO SECTION
-catchy = Section.create name: 'catchy', order: 3, website: website
-Element.create category: 'list', name: 'interesting', value: "Teacher for 10+ years", section: catchy
-Element.create category: 'video', name: 'video', value: 'https://www.youtube.com/watch?v=TTemFSm_KIU', section: catchy
+# catchy = Section.create name: 'catchy', order: 3, website: website
+# Element.create category: 'list', name: 'interesting', value: "Teacher for 10+ years", section: catchy
+# Element.create category: 'video', name: 'video', value: 'https://www.youtube.com/watch?v=TTemFSm_KIU', section: catchy
 
 # BIO SECTION
-bio = Section.create(name: 'bio', order: 2, website: website)
-Element.create(category: 'h2', name: 'greeting', value: 'Hi! Im Rachel', section: bio)
-Element.create(category: 'p', name: "intro", value: 'I have been teaching for 10 years and guide students to achieve their own goals', section: bio)
-Element.create(category: 'img', name: 'picture', value: 'https://avatars3.githubusercontent.com/u/4753571?v=4', section: bio)
+# bio = Section.create(name: 'bio', order: 2, website: website)
+# Element.create(category: 'h2', name: 'greeting', value: 'Hi! Im Rachel', section: bio)
+# Element.create(category: 'p', name: "intro", value: 'I have been teaching for 10 years and guide students to achieve their own goals', section: bio)
+# Element.create(category: 'img', name: 'picture', value: 'https://avatars3.githubusercontent.com/u/4753571?v=4', section: bio)
 
 # PRICING SECTION
-price_info = Section.create( name: 'pricing', order: 4, website: website)
-Element.create category: "h2", name: "teaser", value: "Book 10+ hours and get 5% off", section: price_info
-Element.create category: "p", name: "policy", value: "Group classes are for a max of 2 students ", section: price_info
-Element.create category: "product", name: "product1", value: "{type: 'one 2 one', offline_price: '20 Eur', online_price: '10 Eur' }", section: price_info
-Element.create category: "product", name: "product2", value: "{type: 'group', offline_price: '20 Eur', online_price: '10 Eur' }", section: price_info
+# price_info = Section.create( name: 'pricing', order: 4, website: website)
+# Element.create category: "h2", name: "teaser", value: "Book 10+ hours and get 5% off", section: price_info
+# Element.create category: "p", name: "policy", value: "Group classes are for a max of 2 students ", section: price_info
+# Element.create category: "product", name: "product1", value: "{type: 'one 2 one', offline_price: '20 Eur', online_price: '10 Eur' }", section: price_info
+# Element.create category: "product", name: "product2", value: "{type: 'group', offline_price: '20 Eur', online_price: '10 Eur' }", section: price_info
 
 # CALENDAR SECTION
-calendar = Section.create( name: 'calendar', order: 5, website: website)
-Element.create category: "p", name: "headline", value: "Let's talk!", section: calendar
-Element.create category: "p", name: "subheadline", value: "I speak English, Spanish and French", section: calendar
-Element.create category: "p", name: "keypoints", value: "Book a time for us to talk, -Assess your level of English, -Set your learning goals", section: calendar
-Element.create category: "p", name: "buttontext", value: "View my calendar", section: calendar
-Element.create category: 'link', name: 'calendlylink', value: 'https://calendly.com/popjen/15min', section: calendar
-
+# calendar = Section.create( name: 'calendar', order: 5, website: website)
+# Element.create category: "p", name: "headline", value: "Let's talk!", section: calendar
+# Element.create category: "p", name: "subheadline", value: "I speak English, Spanish and French", section: calendar
+# Element.create category: "p", name: "keypoints", value: "Book a time for us to talk, -Assess your level of English, -Set your learning goals", section: calendar
+# Element.create category: "p", name: "buttontext", value: "View my calendar", section: calendar
+# Element.create category: 'link', name: 'calendlylink', value: 'https://calendly.com/popjen/15min', section: calendar
 
 #CONTACT SECTION
-contact = Section.create( name: 'contact', order: 6, website: website)
-Element.create category: "email", name: "Email", value: "rachel@gmail.com", section: contact
-Element.create category: "phone_number", name: "Phone Number", value: "+34-123-123456", section: contact
-Element.create category: "link", name: "Facebook", value: "https://www.facebook.com/truonganxinh", section: contact
-Element.create category: "link", name: "Instagram", value: "https://instagram.com/mirandakerr?igshid=i8lvpcvwmmmi", section: contact
-Element.create category: "link", name: "Twitter", value: "https://instagram.com/mirandakerr?igshid=i8lvpcvwmmmi", section: contact
-Element.create category: "link", name: "Linkedin", value: "https://www.linkedin.com/in/lutfur84/", section: contact
-Element.create category: "phone_number", name: "Whatsapp", value: "+34-123-123456", section: contact
+# contact = Section.create( name: 'contact', order: 6, website: website)
+# Element.create category: "email", name: "Email", value: "rachel@gmail.com", section: contact
+# Element.create category: "phone_number", name: "Phone Number", value: "+34-123-123456", section: contact
+# Element.create category: "link", name: "Facebook", value: "https://www.facebook.com/truonganxinh", section: contact
+# Element.create category: "link", name: "Instagram", value: "https://instagram.com/mirandakerr?igshid=i8lvpcvwmmmi", section: contact
+# Element.create category: "link", name: "Twitter", value: "https://instagram.com/mirandakerr?igshid=i8lvpcvwmmmi", section: contact
+# Element.create category: "link", name: "Linkedin", value: "https://www.linkedin.com/in/lutfur84/", section: contact
+# Element.create category: "phone_number", name: "Whatsapp", value: "+34-123-123456", section: contact
 
 puts "done"
-
-# WebsiteSection.create!(
-#   website: poppysworld, section: hero
-# )
-
-# WebsiteSection.create!(
-#   website: poppysworld, section: bio
-# )
-
-# WebsiteSection.create!(
-#   website: poppysworld, section: catchy
-# )
-
-# WebsiteSection.create!(
-#   website: poppysworld, section: price_info
-# )
