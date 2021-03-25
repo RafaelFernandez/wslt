d13ddaaa283c825a39f099923a45df882e81f0a1
Rafael Fernandez <rafinsky@gmail.com>
2021-03-25 13:59:44 +0100

    Publish web

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :websites, only: [:index, :create, :new, :show, :edit] do
+  resources :websites, only: [:index, :create, :new, :show, :update] do
 
     get 'builder', to: 'websites#builder'
     resources :sections, only: [:index, :create, :update, :new, :show]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_165751) do
+ActiveRecord::Schema.define(version: 2021_03_25_100820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 2021_03_23_165751) do
     t.bigint "theme_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "published", default: false
     t.index ["theme_id"], name: "index_websites_on_theme_id"
     t.index ["user_id"], name: "index_websites_on_user_id"
   end
